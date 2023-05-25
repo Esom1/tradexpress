eb7f8fad0aca81da4107bd08eaea509ef8f3c22b
christy <christinaugoala@gmail.com>
2023-05-25 12:39:08 +0100

    footer correction

--- src/FooterDb.js
+++ src/FooterDb.js
@@ -1,7 +1,12 @@
+import twitter from './assets/twitter (2).png'
+import play from './assets/play.png'
+import whatsapp from './assets/whatsapp.png'
+import facebook from './assets/facebook.png'
+
 export const dataFooter =[
   {id: 1, title: 'Product', educate:'Bitcoin', reach: 'Alt', company: 'Flat', rights: 'Refil', last:'P2P'},
   {id:2,title:'Learn', educate:'Blog', reach: 'Help Center'},
-  {id:3, title: 'Contact', educate:'hello@tradexpress.com', reach: 'support@tradexpress.com'},
+  {id:3, title: 'Contact', educate:'hello@tradexpress.com', reach: 'support@tradexpress.com',img:[twitter,play,whatsapp,facebook]},
   {id:4, title: 'Company', educate:'About Us', reach: 'Careers', company: 'Rates', rights: 'Mobile'},
   {id:5, title: 'Legal', educate:'Privacy Policy', reach: 'Anti-Money Laundering', company: 'Terms and Conditions'},
 ]
--- src/Layout/Footer.js
+++ src/Layout/Footer.js
@@ -1,18 +1,20 @@
-import React from 'react'
-import { dataFooter } from '../FooterDb'
-import '../styles/Footer.css'
-
+import React from "react";
+import { dataFooter } from "../FooterDb";
+import "../styles/Footer.css";
+import copyright from '../assets/copyright.png'
 
 const Footer = () => {
+  console.log(dataFooter[2].img[1]);
   return (
-    <div className='footer mt-5'>
-      <div className='container d-lg-flex justify-content-between align-items-center'>
-      {dataFooter.map((datumFooter)=>{
-          const { id,title,educate,reach,company,rights,last } = datumFooter
-          return(
-            <div className='footer-div'key={id}>
+    <div className="footer mt-5">
+      <div className="container d-lg-flex justify-content-between ">
+        {dataFooter.map((datumFooter) => {
+          const { id, title, educate, reach, company, rights, last } =
+            datumFooter;
+          return (
+            <div className="footer-div mt-5" key={id}>
               <p> {title} </p>
-              <ul className=' list-unstyled lh-lg'>
+              <ul className=" list-unstyled lh-lg">
                 <li>{educate}</li>
                 <li>{reach}</li>
                 <li>{company}</li>
@@ -20,13 +22,25 @@ const Footer = () => {
                 <li>{last}</li>
               </ul>
             </div>
-          )
+          );
         })}
-
       </div>
-      
+      <div className="d-flex justify-content-center gap-4 position-relative footer-image">
+        <img src={dataFooter[2].img[0]} alt="" />
+        <img src={dataFooter[2].img[1]} alt="" />
+        <img src={dataFooter[2].img[2]} alt="" />
+        <img src={dataFooter[2].img[3]} alt="" />
+      </div>
+      <div className="container d-flex gap-1 justify-content-center end">
+        <div className="last-image">
+          <img src={copyright} alt="" />
+        </div>
+        <div>
+          <p>2021 Tradexpress Technologies. All Right Reserved</p>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
