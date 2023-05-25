d3924f69f5e5527779573e640066194262ce7bcc
christy <christinaugoala@gmail.com>
2023-05-25 03:02:53 +0100

    i imported footer

---
+++ src/FooterDb.js
@@ -0,0 +1,7 @@
+export const dataFooter =[
+  {id: 1, title: 'Product', educate:'Bitcoin', reach: 'Alt', company: 'Flat', rights: 'Refil', last:'P2P'},
+  {id:2,title:'Learn', educate:'Blog', reach: 'Help Center'},
+  {id:3, title: 'Contact', educate:'hello@tradexpress.com', reach: 'support@tradexpress.com'},
+  {id:4, title: 'Company', educate:'About Us', reach: 'Careers', company: 'Rates', rights: 'Mobile'},
+  {id:5, title: 'Legal', educate:'Privacy Policy', reach: 'Anti-Money Laundering', company: 'Terms and Conditions'},
+]
--- src/Layout/Footer.js
+++ src/Layout/Footer.js
@@ -1,8 +1,30 @@
 import React from 'react'
+import { dataFooter } from '../FooterDb'
+
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='footer mt-5'>
+      <div className='container d-lg-flex justify-content-between align-items-center'>
+      {dataFooter.map((datumFooter)=>{
+          const { id,title,educate,reach,company,rights,last } = datumFooter
+          return(
+            <div className='fs-small footer-div'key={id}>
+              <h2> {title} </h2>
+              <ul className=' list-unstyled lh-lg'>
+                <li>{educate}</li>
+                <li>{reach}</li>
+                <li>{company}</li>
+                <li>{rights}</li>
+                <li>{last}</li>
+              </ul>
+            </div>
+          )
+        })}
+
+      </div>
+      
+    </div>
   )
 }
 
